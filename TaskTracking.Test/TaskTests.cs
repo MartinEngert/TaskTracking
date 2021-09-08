90324c403fc30ea25b1a3d2c3a068b826b7120a8
Martin EngertM <engertmartin@web.de>
2021-09-08 12:12:52 +0200

    Hinzufügen eines Testprojektes, auf Grundlage von XUnit. (Alternativen wären NUnit bzw. MSTest)

---
+++ TaskTracking.Test/TaskTests.cs
@@ -0,0 +1,15 @@
+﻿using Xunit;
+
+namespace TaskTracking.Test
+{
+    public class TaskTests
+    {
+        [Fact]
+        public void TestMe()
+        {
+            // Arrange
+            // Act
+            // Assert
+        }
+    }
+}
